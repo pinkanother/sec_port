4ba23d55cbcd8c7333d3feccba6c3b17fd1fe907
pinkanother <pinkanother@gmail.com>
2025-02-18 12:58:48 -0500

    Feb18_4

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,7 +17,7 @@ const About = () => (
       <div className="row gy-4 justify-content-center">
         <div className="col-lg-4 text-center">
           <img
-            src="image/James.jpg"
+            src="/image/James.jpg"
             className="img-fluid"
             style={{
               maxWidth: "80%",
